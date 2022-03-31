181faf4df67fd025b6095fa33ea26aa3d5a37421
lsw <454412569@qq.om>
2022-03-31 21:04:51 +0800

    手写new

---
+++ 手写代码/03_手写new.js
@@ -0,0 +1,28 @@
+/**
+ * 调用new的过程主要会发生一下4件事
+ * 1. 首先创建了一个空的对象
+ * 2. 设置原型，将对象的原型设置为函数的prototype对象
+ * 3. 让函数的this指向这个对象，执行构造函数的代码
+ * 4. 判断函数的返回值类型，如果是值类型，返回创建的对象；如果是引用类型，返回这个引用类型的对象
+ */
+
+function objectFactory() {
+  let newObject = null;
+  let constructor = Array.prototype.shift.call(arguments);
+  let result = null;
+  // 判断参数是否是一个函数
+  if (typeof constructor !== "function") {
+    console.error("type error");
+    return;
+  }
+  // 新建一个空对象，对象的原型为构造函数的 prototype 对象
+  newObject = Object.create(constructor.prototype);
+  // 将 this 指向新建对象，并执行函数
+  result = constructor.apply(newObject, arguments);
+  // 判断返回对象
+  let flag = result && (typeof result === "object" || typeof result === "function");
+  // 判断返回结果
+  return flag ? result : newObject;
+}
+// 使用方法
+objectFactory(构造函数, 初始化参数);
