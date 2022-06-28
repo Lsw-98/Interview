98c2799ecffbe5607c13e9cf7e5d2f17a3503323
lsw <454412569@qq.om>
2022-06-28 22:55:00 +0800

    复习js

--- 手写代码/demo.js
+++ 手写代码/demo.js
@@ -30,9 +30,27 @@
 //   console.log(res) // [1, 2, null, 4]
 // })
 
-Promise.all([]).then(res => {
-  console.log("all");
-})
-Promise.race([]).then(res => {
-  console.log("race");
-})
+
+
+function bar() {
+  var myName = "极客世界"
+  let test1 = 100
+  if (1) {
+    let myName = "Chrome浏览器"
+    console.log(test)
+  }
+}
+
+function foo() {
+  var myName = "极客邦"
+  let test = 2
+  {
+    let test = 3
+    bar()
+  }
+}
+
+var myName = "极客时间"
+let myAge = 10
+let test = 1
+foo()
